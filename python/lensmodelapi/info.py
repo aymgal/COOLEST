2e2f12e47d04edf9051cee35545c7b17f744271b
Aymeric Galan <aymeric.galan@gmail.com>
2021-09-09 14:25:52 +0200

    Add small info module for quick access to useful info such as all supported profiles

---
+++ python/lensmodelapi/info.py
@@ -0,0 +1,7 @@
+# Some variables useful to access
+
+from lensmodelapi.profiles import light
+supported_light_profiles = light.SUPPORTED_PROFILES
+
+from lensmodelapi.profiles import mass
+supported_mass_profiles = mass.SUPPORTED_PROFILES
