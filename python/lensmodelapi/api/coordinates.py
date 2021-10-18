d183a90109d5c6f59614e64f151326d23df8e45a
Aymeric Galan <aymeric.galan@gmail.com>
2021-10-18 16:45:15 +0200

    Minor variable name change

--- python/lensmodelapi/api/coordinates.py
+++ python/lensmodelapi/api/coordinates.py
@@ -5,21 +5,21 @@ from lensmodelapi.api.base import APIBaseObject
 
 class Coordinates(APIBaseObject):
 
-    _ra_orientations = ['left', 'right']
-    _dec_orientations = ['top', 'bottom']
+    _orientations_ra = ['left', 'right']
+    _orientations_dec = ['top', 'bottom']
     _origins = ['center', 'bottom_left']
 
     def __init__(self, 
-                 ra_orientation: str = 'left', 
-                 dec_orientation: str = 'top',
+                 orientation_ra: str = 'left', 
+                 orientation_dec: str = 'top',
                  origin: str = 'center',
                  ) -> None:
-        if ra_orientation not in self._ra_orientations:
-            raise ValueError(f"RA orientation can only be in {self._ra_orientations}.")
-        self.ra_orientation = ra_orientation
-        if dec_orientation not in self._dec_orientations:
-            raise ValueError(f"Dec orientation can only be in {self._dec_orientations}.")
-        self.dec_orientation = dec_orientation
+        if orientation_ra not in self._orientations_ra:
+            raise ValueError(f"RA orientation can only be in {self._orientations_ra}.")
+        self.orientation_ra = orientation_ra
+        if orientation_dec not in self._orientations_dec:
+            raise ValueError(f"Dec orientation can only be in {self._orientations_dec}.")
+        self.orientation_dec = orientation_dec
         if origin not in self._origins:
             raise ValueError(f"Dec orientation can only be in {self._origins}.")
         self.origin = origin
