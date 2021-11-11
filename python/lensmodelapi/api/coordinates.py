4d420f2873263d531621c1adb9bd2bb796df7d6a
Aymeric Galan <aymeric.galan@gmail.com>
2021-11-11 15:11:27 +0100

    Add angle orientation and pixel positioning attributes to Coordinates

--- python/lensmodelapi/api/coordinates.py
+++ python/lensmodelapi/api/coordinates.py
@@ -7,12 +7,16 @@ class Coordinates(APIBaseObject):
 
     _orientations_ra = ['left', 'right']
     _orientations_dec = ['top', 'bottom']
-    _origins = ['center', 'bottom left']
+    _origins = ['center', 'bottom-left']
+    _angles = ['east-to-north', 'west-to-north', 'east-to-south', 'west-to-south']
+    _coord_in_pixel = ['center', 'bottom-left']
 
     def __init__(self, 
                  orientation_ra: str = 'left', 
                  orientation_dec: str = 'top',
                  origin_position: str = 'center',
+                 angles_orientation: str = 'east-to-north',
+                 coordinate_in_pixel: str = 'center',
                  ) -> None:
         if orientation_ra not in self._orientations_ra:
             raise ValueError(f"RA orientation can only be in {self._orientations_ra}.")
@@ -21,8 +25,15 @@ class Coordinates(APIBaseObject):
             raise ValueError(f"Dec orientation can only be in {self._orientations_dec}.")
         self.orientation_dec = orientation_dec
         if origin_position not in self._origins:
-            raise ValueError(f"Dec orientation can only be in {self._origins}.")
+            raise ValueError(f"Position of the origin can only be in {self._origins}.")
         self.origin_position = origin_position
+        if angles_orientation not in self._angles:
+            raise ValueError(f"Angle orientation can only be in {self._angles}.")
+        self.angles_orientation = angles_orientation
+        if coordinate_in_pixel not in self._coord_in_pixel:
+            raise ValueError(f"Coordinate position inside a given pixel "
+                             "can only be in {self._coord_in_pixel}.")
+        self.coordinate_in_pixel = coordinate_in_pixel
         super().__init__()
 
     def update_with_instrument(self, instrument):
