b81e51ceb99cb8625534cec070df6a96254d796f
Aymeric Galan <aymeric.galan@gmail.com>
2021-12-02 16:13:12 +0100

    Improve method for basic JSON serialization

--- python/lensmodelapi/io.py
+++ python/lensmodelapi/io.py
@@ -4,7 +4,7 @@ import json
 import jsonpickle
 
 
-class APIHierarchy(object):
+class APISerializer(object):
 
     def __init__(self,
                  file_path_no_ext: str, 
@@ -13,36 +13,45 @@ class APIHierarchy(object):
         self.obj = obj
         self.path = file_path_no_ext
         self.indent = indent
+        # to distinguish files that can be converted back to the python API
+        self._api_suffix = '_pyAPI'
 
-    def yaml_dump(self):
-        yaml_path = self.path + '.yaml'
-        with open(yaml_path, 'w') as f:
-            result = yaml.dump(self.obj, f, indent=self.indent,
-                               sort_keys=False, default_flow_style=False)
+    def json_dump_simple(self):
+        json_path = self.path + '.json'
+        result = jsonpickle.encode(self.obj, indent=self.indent)
+        with open(json_path, 'w') as f:
+            f.write(self.obj.to_JSON(indent=self.indent))
         return result
 
     def json_dump(self):
-        json_path = self.path + '.json'
+        json_path = self.path + self._api_suffix + '.json'
         result = jsonpickle.encode(self.obj, indent=self.indent)
         with open(json_path, 'w') as f:
             f.write(result)
         return result
 
-    def yaml_load(self):
-        yaml_path = self.path + '.yaml'
-        with open(yaml_path, 'r') as f:
-            content = yaml.load(f, Loader=yaml.Loader)
-        return content
+    def yaml_dump(self):
+        yaml_path = self.path + self._api_suffix + '.yaml'
+        with open(yaml_path, 'w') as f:
+            result = yaml.dump(self.obj, f, indent=self.indent,
+                               sort_keys=False, default_flow_style=False)
+        return result
 
     def json_load(self):
-        json_path = self.path + '.json'
+        json_path = self.path + self._api_suffix + '.json'
         with open(json_path, 'r') as f:
             content = jsonpickle.decode(f.read())
         return content
 
+    def yaml_load(self):
+        yaml_path = self.path + self._api_suffix + '.yaml'
+        with open(yaml_path, 'r') as f:
+            content = yaml.load(f, Loader=yaml.Loader)
+        return content
+
     def dump_yaml_to_json(self):
-        yaml_path = self.path + '.yaml'
-        json_path = self.path + '.json'
+        yaml_path = self.path + self._api_suffix + '.yaml'
+        json_path = self.path + self._api_suffix + '.json'
         def _any_constructor(loader, tag_suffix, node):
             """https://stackoverflow.com/questions/52240554/how-to-parse-yaml-using-pyyaml-if-there-are-within-the-yaml"""
             if isinstance(node, yaml.MappingNode):
