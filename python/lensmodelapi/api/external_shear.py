ca29773b8e597bfea784ce5b1b8a1f873eee3306
Aymeric Galan <aymeric.galan@gmail.com>
2021-12-02 16:11:37 +0100

    Improve generation of unique IDs for profiles and parameters

--- python/lensmodelapi/api/external_shear.py
+++ python/lensmodelapi/api/external_shear.py
@@ -2,6 +2,7 @@ __author__ = 'aymgal'
 
 from lensmodelapi.api.base import APIBaseObject
 from lensmodelapi.api.mass_light_model import MassModel
+from lensmodelapi.api import util
 
 
 class ExternalShear(APIBaseObject):
@@ -18,3 +19,12 @@ class ExternalShear(APIBaseObject):
             mass_model = MassModel()
         self.mass_model = mass_model
         super().__init__()
+        self._create_all_ids()
+
+    def _create_all_ids(self):
+        for j, profile in enumerate(self.mass_model.profiles):
+            profile_id = util.ext_shear_profile_to_id(profile.name, j)
+            profile.id = profile_id
+            for param_name, parameter in profile.parameters.items():
+                param_id = f'{profile.id}-{param_name}'
+                parameter.id = param_id
--- python/lensmodelapi/api/galaxy_list.py
+++ python/lensmodelapi/api/galaxy_list.py
@@ -20,19 +20,12 @@ class GalaxyList(list, APIBaseObject):
     def num_lenses():
         raise NotImplementedError("Couting number of source galaxies is not yet implemented.")
 
-    def update_parameter_bounds_with_obs(self, instrument):
-        for galaxy in self:
-            for light_profile in galaxy.light_model.profiles:
-                light_profile.update_parameter_bounds_with_obs(instrument)
-            for mass_profile in galaxy.mass_model.profiles:
-                mass_profile.update_parameter_bounds_with_obs(instrument)
-
     def _create_all_ids(self):
         for i, galaxy in enumerate(self):
             for model_type in ['light', 'mass']:
                 for j, profile in enumerate(getattr(galaxy, f'{model_type}_model').profiles):
-                    profile_id = util.profile_to_id(model_type, profile.name, j, i)
+                    profile_id = util.galaxy_profile_to_id(model_type, profile.name, j, i)
                     profile.id = profile_id
                     for param_name, parameter in profile.parameters.items():
-                        param_id = f'{profile.id}_{param_name}'
+                        param_id = f'{profile.id}-{param_name}'
                         parameter.id = param_id
