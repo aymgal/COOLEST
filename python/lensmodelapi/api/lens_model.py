61e723bb0574f91e951932b74a208434df15e529
Aymeric Galan <aymeric.galan@gmail.com>
2022-03-09 17:30:01 +0100

    Add coordinates origin to the template

--- python/lensmodelapi/api/lens_model.py
+++ python/lensmodelapi/api/lens_model.py
@@ -7,6 +7,7 @@ from lensmodelapi.api.galaxy_list import GalaxyList
 from lensmodelapi.api.external_shear import ExternalShear
 from lensmodelapi.api.regularization_list import RegularizationList
 from lensmodelapi.api.likelihood_list import LikelihoodList
+from lensmodelapi.api.coordinates import CoordinatesOrigin
 from lensmodelapi.api.cosmology import Cosmology
 
 
@@ -14,13 +15,15 @@ class LensModel(APIBaseObject):
 
     def __init__(self, 
                  galaxies: GalaxyList,
-                 likelihoods: LikelihoodList = None,
                  external_shear: ExternalShear = None,
+                 coordinates_origin: CoordinatesOrigin = None,
+                 likelihoods: LikelihoodList = None,
                  regularizations: RegularizationList = None,
                  cosmology: Cosmology = None) -> None:
         self.galaxies = galaxies
-        if likelihoods is None:
-            likelihoods = LikelihoodList('imaging_data')
+        if coordinates_origin is None:
+            coordinates_origin = CoordinatesOrigin('0', '0')
+        self.coordinates_origin = coordinates_origin
         self.likelihoods = likelihoods
         self.external_shear = external_shear
         self.regularizations = regularizations
