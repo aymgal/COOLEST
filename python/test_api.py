8e61ed664d1a5b0736ee23a40b0869b40da9913d
Aymeric Galan <aymeric.galan@gmail.com>
2022-03-09 17:31:55 +0100

    Update API test script

--- python/test_api.py
+++ python/test_api.py
@@ -8,33 +8,9 @@ from lensmodelapi.io import APISerializer
 
 from pprint import pprint
 
-# Provide data file
-data = Data(FitsFile('test_image.fits'),
-            noise_map=None,
-            wht_map=None,
-            arc_mask=FitsFile('arc_mask.fits'),
-            likelihood_mask=None,
-            time_delays=[10., 20., 40.],
-            magnification_ratios=None)
 
-# Defines the instrument
-instrument = Instrument('some instrument',
-                        FitsFile('test_psf.fits'),
-                        pixel_size=0.08, 
-                        field_of_view_ra=None,
-                        field_of_view_dec=None,
-                        exposure_time=1000,
-                        background_rms=0.05,
-                        psf_pixel_size=None)
+############## LENS MODELING ################
 
-# estimate background noise from data
-instrument.set_background_rms(data.estimate_background_noise())
-
-# check consistency between data and instrument
-# data.check_consistency_with_instrument(instrument)
-
-# or update instrument field-of-view based on data
-instrument.update_fov_with_data(data)
 
 # Setup cosmology
 cosmology = Cosmology(H0=73.0, Om0=0.3)
@@ -70,45 +46,41 @@ regularization_list = RegularizationList(('PixelStarlet', source_2.light_model.p
 # Choose which likelihood terms you want to include
 likelihood_list = LikelihoodList('imaging_data')
 
-# TESTS
-# - add a gaussian prior to a given parameter
-from lensmodelapi.api.probabilities import GaussianPrior
-lens_1.mass_model.profiles[0].parameters['gamma'].set_prior(GaussianPrior(mean=2.0, width=0.2))
+# Define the origin of the coordinates system
+origin = CoordinatesOrigin('00h11m20.244s', '-08d45m51.48s')  # <- in degrees (2.83435, )
 
-# - add a point estimate to a given parameter
-from lensmodelapi.api.parameter import PointEstimate
-lens_1.light_model.profiles[1].parameters['q'].set_point_estimate(PointEstimate(value=0.89))
+# # TESTS
+# # - add a gaussian prior to a given parameter
+# from lensmodelapi.api.probabilities import GaussianPrior
+# lens_1.mass_model.profiles[0].parameters['gamma'].set_prior(GaussianPrior(mean=2.0, width=0.2))
 
-# - add a posterior distribution (as 0th and 1st order statistics)
-from lensmodelapi.api.probabilities import PosteriorStatistics
-source_1.light_model.profiles[0].parameters['R_sersic'].set_posterior(PosteriorStatistics(mean=0.12, median=0.15, 
-                                                                                          percentile_16th=0.03, percentile_84th=0.05))
+# # - add a point estimate to a given parameter
+# from lensmodelapi.api.parameter import PointEstimate
+# lens_1.light_model.profiles[1].parameters['q'].set_point_estimate(PointEstimate(value=0.89))
+
+# # - add a posterior distribution (as 0th and 1st order statistics)
+# from lensmodelapi.api.probabilities import PosteriorStatistics
+# source_1.light_model.profiles[0].parameters['R_sersic'].set_posterior(PosteriorStatistics(mean=0.12, median=0.15, 
+#                                                                                           percentile_16th=0.03, percentile_84th=0.05))
 
 # Define the LensModel that merges physical objects (galaxies), 
 # regularization strategies and a choice of coordinate system
 lens_model = LensModel(galaxy_list,
                        external_shear=ext_shear,
+                       coordinates_origin=origin,
                        regularizations=regularization_list,
                        likelihoods=likelihood_list,
                        cosmology=cosmology)
 
 # Assign the list of galaxies to a LensObject
 # along with the coordinate system and (optinonally) the observation
-lens_object_1 = LensObject('My Favorite Lens ever',
-                           instrument,
-                           lens_model,
-                           data=data)
+lens_object_1 = LensObject('My Favorite Lens ever', lens_model)
 
-# lens_object_2 = LensObject('Same system in another band',
-#                            instrument,
-#                            lens_model,
-#                            data=data_2)
+# lens_object_2 = LensObject('Same system in another band for instance', lens_model)
 
-# can do another LensObject just the same... and build a LensSample
+# Wrap up a list of objects (here, only one) into a LensSample master object
 lens_sample = LensSample(lens_object_1)  # , lens_object_2, lens_object_3, ...
-
-# Wrap up a list of objects (here, only one) and a cosmology into a LensUniverse master object
-print("FINAL OBJECT\n", lens_sample.__dict__, '\n')
+print("FINAL OBJECT\n", lens_sample, '\n')
 
 # print supported profiles so far
 print("Supported choices:")
@@ -121,21 +93,65 @@ print("#"*30 + " serialization " + "#"*30)
 # print(json.dumps(lens_1.mass_model.profiles[1].parameters, cls=JSONParameter, indent=4))
 
 # TESTS WITH YAML
-encoder_yaml = APISerializer('api_input_file_YAML', obj=lens_sample, indent=2)
-encoder_yaml.yaml_dump()
-encoder_yaml.dump_yaml_to_json()
+sample_encoder_yaml = APISerializer('api_input_file_YAML', obj=lens_sample, indent=2)
+sample_encoder_yaml.yaml_dump()
+sample_encoder_yaml.dump_yaml_to_json()
 # test construct class from YAML
-lens_sample_2 = encoder_yaml.yaml_load()
-print("Retrieved object is a LensUniverse instance?", 
-      isinstance(lens_sample_2, LensUniverse))
+lens_sample_2 = sample_encoder_yaml.yaml_load()
+print("Retrieved object is a LensSample instance?", 
+      isinstance(lens_sample_2, LensSample))
 print("First lens in the sample:", lens_sample[0].name)
 
 # TESTS WITH JSON
-encoder_json = APISerializer('api_input_file_JSON', obj=lens_sample, indent=2)
-encoder_json.json_dump()
-encoder_json.json_dump_simple()
-lens_sample_3 = encoder_json.json_load()
-print("Retrieved object is a LensUniverse instance?", 
-      isinstance(lens_sample_3, LensUniverse))
+sample_encoder_json = APISerializer('api_input_file_JSON', obj=lens_sample, indent=2)
+sample_encoder_json.json_dump()
+sample_encoder_json.json_dump_simple()
+lens_sample_3 = sample_encoder_json.json_load()
+print("Retrieved object is a LensSample instance?", 
+      isinstance(lens_sample_3, LensSample))
 print("First lens in the sample:", lens_sample[0].name)
+
+
+
+
+
+
+############## OBSERVATIONAL DATA ################
+
+
+# Provide data file
+data = Data(FitsFile('test_image.fits'),
+            noise_map=None,
+            wht_map=None,
+            arc_mask=FitsFile('arc_mask.fits'),
+            likelihood_mask=None,
+            time_delays=[10., 20., 40.],
+            magnification_ratios=None)
+
+# Defines the instrument
+instrument = Instrument('some instrument',
+                        FitsFile('test_psf.fits'),
+                        pixel_size=0.08, 
+                        field_of_view_ra=None,
+                        field_of_view_dec=None,
+                        exposure_time=1000,
+                        background_rms=0.05,
+                        psf_pixel_size=None)
+
+# estimate background noise from data
+instrument.set_background_rms(data.estimate_background_noise())
+
+# check consistency between data and instrument
+# data.check_consistency_with_instrument(instrument)
+
+# or update instrument field-of-view based on data
+instrument.update_fov_with_data(data)
+
+# group as an observation
+observation = Observation(data, instrument)
+
+# save it in a JSON file
+data_encoder_json = APISerializer('api_obs_file_JSON', obj=observation, indent=2)
+data_encoder_json.json_dump_simple()
+
 
