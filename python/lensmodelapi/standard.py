9b4b1f0652dbf0f275f930cc04253719b0173566
Aymeric Galan <aymeric.galan@gmail.com>
2022-05-31 11:12:31 +0200

    Add CoolestStandard master object missing from previous commits

---
+++ python/lensmodelapi/standard.py
@@ -0,0 +1,42 @@
+__author__ = 'aymgal'
+
+
+from lensmodelapi.api.base import APIBaseObject
+from lensmodelapi.api.lens_model import LensModel
+from lensmodelapi.api.observation import Observation
+from lensmodelapi.api.instrument import Instrument
+from lensmodelapi.api.cosmology import Cosmology
+
+
+EXCLUDE_KEYS = [
+    'documentation',
+    'id',
+    'latex_str',
+    'units',
+    'fixed',
+    'definition_range',
+]
+
+
+class CoolestStandard(APIBaseObject):
+
+    _exclude_keys = EXCLUDE_KEYS
+
+    def __init__(self,
+                 lens_model: LensModel,
+                 observation: Observation,
+                 instrument: Instrument,
+                 cosmology: Cosmology = None):
+        self.lens_model  = lens_model
+        self.observation = observation
+        self.instrument  = instrument
+        self.cosmology   = cosmology
+        self.standard = 'coolest'
+        if observation.image.exists:
+            mode = 'model'
+        else:
+            mode = 'mock'
+        self.meta = {
+            'mode': mode,
+        }
+        
