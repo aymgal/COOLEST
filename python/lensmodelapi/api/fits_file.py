d3b67a1358142138205c9949d44edc41fd0f2f01
Aymeric Galan <aymeric.galan@gmail.com>
2022-09-09 16:13:32 +0200

    Add more specific classes for fits file objects

--- python/lensmodelapi/api/fits_file.py
+++ python/lensmodelapi/api/fits_file.py
@@ -6,12 +6,27 @@ from astropy.io import fits
 from lensmodelapi.api.base import APIBaseObject
 
 
-class PixelFitsFile(APIBaseObject):
-    """A simple FITS file"""
-    def __init__(self,
-                 fits_path: str,
-                 pixel_size: float = None) -> None:
+
+class FitsFile(APIBaseObject):
+    """A .fits file object"""
+    def __init__(self, fits_path: str = None) -> None:
         self.fits_path = fits_path
+        super().__init__()
+
+    @property
+    def exists(self):
+        return self.fits_path is not None and os.path.exists(self.fits_path)
+
+    def read(self):
+        return fits.getdata(self.fits_path, header=True)
+
+
+class PixelFitsFile(FitsFile):
+    """A .fits file object holding a 2D image"""
+    def __init__(self,
+                 fits_path: str = None,
+                 pixel_size: float = None) -> None:
+        super().__init__(fits_path)
         self.pixel_size = pixel_size
         if self.exists:
             pixels, header = self.read()
@@ -20,15 +35,14 @@ class PixelFitsFile(APIBaseObject):
             self.num_pix_x, self.num_pix_y = array_shape
         else:
             self.num_pix_x, self.num_pix_y = None, None
-        super().__init__()
-
-    @property
-    def exists(self):
-        return self.fits_path is not None and os.path.exists(self.fits_path)
 
     @property
     def shape(self):
         return (self.num_pix_x, self.num_pix_y)
-    
-    def read(self):
-        return fits.getdata(self.fits_path, header=True)
+
+
+class MultiExtFitsFile(FitsFile):
+    """A multi-extension .fits file object"""
+    def __init__(self,
+                 fits_path: str = None) -> None:
+        super().__init__(fits_path)
