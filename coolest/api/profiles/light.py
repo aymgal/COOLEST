f5c7f0334e51f3a1badeb16a5eee658b7b448309
aymgal <aymeric.galan@gmail.com>
2023-02-22 15:58:30 +0100

    Add interpolation for evaluating regular pixelated light profiles

--- coolest/api/profiles/light.py
+++ coolest/api/profiles/light.py
@@ -2,25 +2,39 @@ __author__ = 'lynevdv'
 
 
 import numpy as np
-from coolest.api.profiles import util
 
+from coolest.api.profiles import util
 
 
 class BaseLightProfile(object):
 
+    _units = None
+
     def surface_brightness(self, **params):
-        raise NotImplementedError()
+        raise NotImplementedError(f"The method surface_brightness() is not defined "
+                                  f"for profile '{self.__class__.__name__}'")
 
     def evaluate_surface_brightness(self, **params):
-        raise NotImplementedError()
+        raise NotImplementedError(f"The method evaluate_surface_brightness() is not defined "
+                                  f"for profile '{self.__class__.__name__}'")
 
     def get_extent(self):
         return None
+
+    @property
+    def units(self):
+        if not hasattr(self, '_units'):
+            raise NotImplementedError(f"Units for profile {self.__class__.__name__} is not defined")
+        if self._units not in ('flux_per_pix', 'flux_per_ang'):
+            raise ValueError(f"Unsupported units type {self._units}")
+        return self._units
 
 
 class Sersic(BaseLightProfile):
 
     """Elliptical Sersic"""
+
+    _units = 'flux_per_ang'
 
     def surface_brightness(self, I_eff=1., theta_eff=2., n=4., phi=0., q=1., center_x=0., center_y=0.):
         raise ValueError("Sersic surface brightness can only be evaluated")
@@ -36,7 +50,10 @@ class PixelatedRegularGrid(BaseLightProfile):
 
     """Pixelated profile on a regular grid"""
 
-    def __init__(self, field_of_view_x, field_of_view_y, num_pix_x, num_pix_y):
+    _units = 'flux_per_pix'
+
+    def __init__(self, field_of_view_x, field_of_view_y, num_pix_x, num_pix_y,
+                 interpolation_method='cubic'):
         if num_pix_x == 0 or num_pix_y == 0:
             raise ValueError("Light profile defined on regular grid has zero pixels")
         self._fov_x = field_of_view_x
@@ -44,6 +61,9 @@ class PixelatedRegularGrid(BaseLightProfile):
         self._nx = num_pix_x
         self._ny = num_pix_y
         self._shape = (num_pix_x, num_pix_y)
+        self._pix_scl_x = np.abs(self._fov_x[0] - self._fov_x[1]) / self._nx
+        self._pix_scl_y = np.abs(self._fov_y[0] - self._fov_y[1]) / self._ny
+        self._interp_method = interpolation_method
 
     def surface_brightness(self, pixels=None):
         """Returns the surface brightness pixels"""
@@ -51,13 +71,22 @@ class PixelatedRegularGrid(BaseLightProfile):
             return np.zeros(self._shape)
         return pixels
 
-    def evaluate_surface_brightness(self, pixels=None):
-        """Returns the surface brightness at the given position (x, y)"""
-        raise NotImplementedError()
+    def evaluate_surface_brightness(self, x, y, pixels=None):
+        extent = self.get_extent()
+        points = (
+            np.linspace(extent[2], extent[3], self._ny, endpoint=True),
+            np.linspace(extent[0], extent[1], self._nx, endpoint=True),
+        )
+        values = pixels
+        interp = util.CartesianGridInterpolator(points, values, 
+                                                method=self._interp_method)
+        points_eval = np.array([y.ravel(), x.ravel()]).T
+        values_eval = interp(points_eval).reshape(*x.shape)
+        return values_eval
 
     def get_extent(self):
-        half_pix_x = np.abs(self._fov_x[0] - self._fov_x[1]) / self._nx / 2.
-        half_pix_y = np.abs(self._fov_y[0] - self._fov_y[1]) / self._ny / 2.
+        half_pix_x = self._pix_scl_x / 2.
+        half_pix_y = self._pix_scl_y / 2.
         return [
             self._fov_x[0] - half_pix_x, 
             self._fov_x[1] + half_pix_x, 
@@ -70,18 +99,15 @@ class IrregularGrid(BaseLightProfile):
 
     """Pixelated profile on an irregular grid of points {x, y, z}"""
 
-    def __init__(self, field_of_view_x, field_of_view_y):
+    _units = 'flux_per_pix'
+
+    def __init__(self, field_of_view_x, field_of_view_y, num_pix):
         self._fov_x = field_of_view_x
         self._fov_y = field_of_view_y
+        self._n = num_pix
 
     def surface_brightness(self, x=None, y=None, z=None):
         """Returns the surface brightness pixels"""
-        if pixels is None:
-            return 0., [0, 0, 0, 0]
-        extent = [self._fov_x[0], self._fov_x[1], self._fov_y[0], self._fov_y[1]]
-        return (x, y, z), extent
-
-    def evaluate_surface_brightness(self, pixels=None):
-        """Returns the surface brightness at the given position (x, y)"""
-        raise NotImplementedError()
-
+        if x is None or y is None or z is None:
+            return 0.
+        return x, y, z
--- coolest/api/profiles/util.py
+++ coolest/api/profiles/util.py
@@ -2,6 +2,7 @@ __author__ = 'aymgal'
 
 
 import numpy as np
+from scipy import ndimage
 
 
 def shift_rotate_elliptical(x, y, phi, q, center_x, center_y):
@@ -13,3 +14,39 @@ def shift_rotate_elliptical(x, y, phi, q, center_x, center_y):
     x_trans = cos_phi * x_shift + sin_phi * y_shift
     y_trans = -sin_phi * x_shift + cos_phi * y_shift
     return x_trans * np.sqrt(q), y_trans / np.sqrt(q)
+
+
+class CartesianGridInterpolator(object):
+    """
+    Regular grid spline interpolator
+    https://docs.scipy.org/doc/scipy/tutorial/interpolate/ND_regular_grid.html#uniformly-spaced-data
+    """
+    
+    def __init__(self, points, values, method='linear', fill_value=0.):
+        self.limits = np.array([[min(x), max(x)] for x in points])
+        self.values = np.asarray(values, dtype=float)
+        self.order = {'linear': 1, 'cubic': 3, 'quintic': 5}[method]
+        self.fill_value = fill_value
+
+    def __call__(self, xi):
+        """
+        `xi` here is an array-like (an array or a list) of points.
+
+        Each "point" is an ndim-dimensional array_like, representing
+        the coordinates of a point in ndim-dimensional space.
+        """
+        # transpose the xi array into the ``map_coordinates`` convention
+        # which takes coordinates of a point along columns of a 2D array.
+        xi = np.asarray(xi).T
+
+        # convert from data coordinates to pixel coordinates
+        ns = self.values.shape
+        coords = [(n-1)*(val - lo) / (hi - lo) 
+                  for val, n, (lo, hi) in zip(xi, ns, self.limits)]
+
+        # interpolate
+        return ndimage.map_coordinates(self.values, coords,
+                                       order=self.order,
+                                       mode='constant',
+                                       cval=self.fill_value,
+                                       prefilter=False)
