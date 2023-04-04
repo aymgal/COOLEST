b71273d47220e5be4b31d7512e5b860279f89cbb
mattgomer <matthewrgomer@gmail.com>
2023-04-04 13:30:37 -0600

    added effective_radial_slope and half_light_radius routines

--- coolest/api/analysis.py
+++ coolest/api/analysis.py
@@ -118,14 +118,113 @@ class Analysis(object):
         else:
             return r_Ein
 
-    def effective_radial_slope(self, **kwargs_selection):
+    def kappa_1d_profile(self, center=None, r_vec=np.linspace(0,10,100),**kwargs_selection):
+        """
+        Calculates 1D profile using the kappa grid.
+
+        :param center: (x, y)-coordinates of the center from which to calculate; if None, use the value from create_kappa_image
+        :param r_vec: range of radii over which to calculate the 1D profile
+        """
         if kwargs_selection is None:
             kwargs_selection = {}
         mass_model = ComposableMassModel(self.coolest, self.coolest_dir, **kwargs_selection)
-        raise NotImplementedError
+        x, y = self.coordinates.pixel_coordinates
+        kappa_image = mass_model.evaluate_convergence(x, y)
+        if center is None:
+            center_x, center_y = mass_model.estimate_center()
+        else:
+            center_x, center_y = center
+        r_grid=np.sqrt((x-center_x)**2+(y-center_y)**2)
+        kappa_profile=np.zeros_like(r_vec)
+        for r_i,r in enumerate(r_vec):
+            if r_i==0:
+                r_min=0
+            else:
+                r_min=r_vec[r_i-1]
+            in_radial_bin=(r_grid > r_min) & (r_grid <= r)
+            kappa_profile[r_i]=np.mean(kappa_image[in_radial_bin])
+        return kappa_profile, r_vec
 
-    def half_light_radius(self, **kwargs_selection):
+    def effective_radial_slope(self, r_eval=None, center=None, r_vec=np.linspace(0,10,100),**kwargs_selection):
+        """
+        Numerically calculates slope of the kappa profile. Because this is defined on a grid, it is not as accurate or robust as
+        an analytical calculation. 
+
+        :param r_eval: float, radius at which to return a single value of the slope (e.g. Einstein radius). 
+                        If None (default), returns slope for all values in r_vec
+        :param center: (x, y)-coordinates of the center from which to calculate; if None, use the value from create_kappa_image
+        :param r_vec: range of radii over which to calculate the 1D profile
+        
+        """
+        
+        kappa_profile, r_vec =self.kappa_1d_profile(center=center, r_vec=r_vec, **kwargs_selection)
+        rise=np.log10(kappa_profile[:-1])-np.log10(kappa_profile[1:])
+        run=np.log10(r_vec[:-1])-np.log10(r_vec[1:])
+        slope=np.append(0,rise/run)#add a zero at r=0 so that slope as same size as r_vec
+        if r_eval==None:
+            return slope 
+        else:
+            closest_r = self.find_nearest(r_vec,r_eval) #just takes closest r. Could rebuild it to interpolate.
+            return slope[r_vec==closest_r]
+            
+    def half_light_radius(self, outer_radius=10, center=None, initial_guess=1, initial_delta_pix=10, 
+                                  n_iter=5,**kwargs_selection):
+        """
+        Numerically calculates effective radius from pixelated surface brightness, using outer_radius as the limit of integration.
+        Not strictly equivalent to effective radius unless outer radius is infinite, which would require an infinite grid and cannot be done with pixelated profiles
+        
+        :param outer_radius: outer limit of integration within which half the light is calculated to estimate the effective radius
+        :param center: (x, y)-coordinates of the center from which to calculate Einstein radius; if None, use the value from create_kappa_image
+        :param initial_guess: initial guess for effective radius, default = 1
+        :param initial_delta_pix: initial step size before shrinking in future iterations, default = 10 pixels
+        :param n_iter: int; number of iterations, default = 5
+        """
+        
         if kwargs_selection is None:
             kwargs_selection = {}
         light_model = ComposableLightModel(self.coolest, self.coolest_dir, **kwargs_selection)
-        raise NotImplementedError
+        # get an image of the convergence
+        x, y = self.coordinates.pixel_coordinates
+        light_image = light_model.evaluate_surface_brightness(x, y)
+
+        # select a center
+        if center is None:
+            center_x, center_y = light_model.estimate_center()
+        else:
+            center_x, center_y = center
+            
+        #initialize
+        grid_res=np.abs(x[0,0]-x[0,1])
+        initial_delta=grid_res*initial_delta_pix #default inital step size is 10 pixels
+        r_grid=np.sqrt((x-center_x)**2+(y-center_y)**2)
+        total_light=np.sum(light_image[r_grid<outer_radius])
+        cumulative_light=np.sum(light_image[r_grid<initial_guess])
+        if cumulative_light < total_light/2: #move outward
+            direction=1
+        elif cumulative_light > total_light/2: #move inward
+            direction=-1
+        else:
+            return initial_guess
+        r_eff=initial_guess
+        delta=initial_delta
+        loopcount=0
+        
+        for n in range(n_iter): #overshoots, turn around and backtrack at higher precision
+            while (total_light/2-cumulative_light)*direction>0: 
+                if loopcount > 100:
+                    raise Warning('Stuck in very long (possibly infinite) loop')
+                    break
+                r_eff=r_eff+delta*direction
+                cumulative_light=np.sum(light_image[r_grid<r_eff])
+                loopcount+=1
+            direction=direction*-1
+            delta=delta/2
+            
+        return r_eff
+
+    
+    def find_nearest(self, array, value):
+        """subfunction to find nearest closest element in array to value"""
+        array = np.asarray(array)
+        idx = (np.abs(array - value)).argmin()
+        return array[idx]
