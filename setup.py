433c73b3fafcdee499e44cbaa9f0c75fa166efcc
LyneVdV <43570542+LyneVdV@users.noreply.github.com>
2023-05-11 10:33:27 +0200

    Update setup.py with url and download_url

--- setup.py
+++ setup.py
@@ -5,4 +5,7 @@ setup(
     packages=['coolest'],
     include_package_data=True,
     install_requires=[],
+    
+    url='https://github.com/aymgal/COOLEST',
+    download_url='https://github.com/aymgal/COOLEST/archive/0.0.0.tar.gz',
 )
