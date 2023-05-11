1f5db46d0cceea678b1d2a75136943cab5ccb921
Lyne.VandeVyvere <Lyne.VandeVyvere@uliege.be>
2023-05-11 10:44:40 +0200

    Revert "Update setup.py with url and download_url"
    This reverts commit 433c73b3fafcdee499e44cbaa9f0c75fa166efcc.

--- setup.py
+++ setup.py
@@ -5,7 +5,4 @@ setup(
     packages=['coolest'],
     include_package_data=True,
     install_requires=[],
-    
-    url='https://github.com/aymgal/COOLEST',
-    download_url='https://github.com/aymgal/COOLEST/archive/0.0.0.tar.gz',
 )
