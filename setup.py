6a61e22c71161df2d6412266cf855d3826bf0dc4
Aymeric Galan <aymeric.galan@gmail.com>
2024-04-17 10:12:47 +0200

    Add pyproject.toml

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ with open(infopath) as open_file:
 this_directory = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
+    readme = f.read()
 
 # Python version
 python_requires = '>=3.7'
